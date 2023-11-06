5f252a17963ab4178bb646ca9fb3b75de4e0c691
Dong TRAN <dong.tt@ebisol.vn>
2023-11-07 00:16:00 +0700

    CI

--- apache-php/resources/entrypoint.sh
+++ apache-php/resources/entrypoint.sh
@@ -4,5 +4,5 @@ printf "Starting PHP daemon...\n"
 php-fpm --daemonize
 printf "Starting Apache2...\n"
 httpd -DFOREGROUND
-# printf "Finished bootstrap\n"
+printf "Finished bootstrap\n"
 # tail -f /var/log/httpd/access.log
