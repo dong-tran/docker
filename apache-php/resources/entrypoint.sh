38874934c97eedfd9d828d76f8a4518e8261ba38
Dong TRAN <dong.tt@ebisol.vn>
2023-11-16 23:08:04 +0700

    add apache build

--- apache-php/resources/entrypoint.sh
+++ apache-php/resources/entrypoint.sh
@@ -3,10 +3,5 @@
 printf "Starting PHP daemon...\n"
 php-fpm --daemonize
 printf "Starting Apache2...\n"
-export APACHE_RUN_DIR=/run/apache2
-export APACHE_PID_FILE=/run/apache2/apache2.pid
-export APACHE_RUN_USER=www-data
-export APACHE_RUN_GROUP=www-data
-export APACHE_LOG_DIR=/var/log/apache2
 
 exec "$@"
